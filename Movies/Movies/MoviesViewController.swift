3121659ede037243196f59a6e858bcc91509b0c5
Dulio Denis <dulio.denis@yahoo.com>
2016-11-28 09:45:08 -0500

    Added a UITableView to the MoviesVC

--- Movies/Movies/MoviesViewController.swift
+++ Movies/Movies/MoviesViewController.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+
+// MARK: - TableView Delegate and Data Source Methods
+
+extension MoviesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        cell.textLabel?.text = "Movie \(indexPath.row + 1)"
+        return cell
+    }
+    
 }
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Movies
-Movies is an iOS 10 App written in Swift 3 that uses the OMDB (Open Movie Database) API to provide a delightful way to look-up and discover movies.
+Movies is an iOS 10 App written in Swift 3 that uses the OMDB ([Open Movie Database](http://www.omdbapi.com/)) API to provide a delightful way to look-up and discover movies.
 
 ![](art/screenshot/movies00.gif?raw=true)
 
-Movies is a rewrite of my Swift 2 [films](https://github.com/duliodenis/films) project.
+Movies is a rewrite of my Swift 2 [Films](https://github.com/duliodenis/films) project.
+
+Movies allows the user to search for movies and uses Swift to parse the JSON returned by the OMDB API to provide the Rotten TomatoMeter Score as well as the rating, release date, and synopsis all with a delightfully simple UX.
 
 ### Contributing
 See [CONTRIBUTING](CONTRIBUTING.md).
