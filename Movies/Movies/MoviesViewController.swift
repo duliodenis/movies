ef1cdc4ae5eaab21039ea0ccf8246cd6a10a49a3
Dulio Denis <dulio.denis@yahoo.com>
2016-11-29 18:53:21 -0500

    Use a URLSession dataTask to download movie JSON from the OMBDAPI

--- Movies/Movies/MoviesViewController.swift
+++ Movies/Movies/MoviesViewController.swift
@@ -11,11 +11,38 @@ import UIKit
 class MoviesViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    var urlOMDB = "https://www.omdbapi.com/?s=Antitrust"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        moviesDownload(urlString: urlOMDB) { (array) in
+            // TODO
+        }
+    }
+    
+    
+    func moviesDownload(urlString: String, completion: (_ array:NSArray)->()) {
+        let url = URL(string: urlString)
+        let session = URLSession.shared
+        let request = session.dataTask(with: url!) { (data, response, error) in
+            if error == nil {
+                if let validData = data {
+                    do {
+                        let jsonDictionary = try JSONSerialization.jsonObject(with: validData, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                        
+                        let movieArray = jsonDictionary["Search"] as! NSArray
+                        print(movieArray)
+                        
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
+        request.resume()
     }
     
 }
